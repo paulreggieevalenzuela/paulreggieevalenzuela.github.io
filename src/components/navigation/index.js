47a94093651357c06dc23417a1222b1adcaa789c
pauvalenzuela <paulreggieevalenzuela@gmail.com>
2020-09-09 02:55:17 +0800

    styles update for mb

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -4,27 +4,12 @@ import PrevLogo from 'assets/images/prev-logo.png';
 
 const Navigation = () => {
 
-    const onClick = (event) => {
-        console.log('test');
-        console.log(event.type); 
-        const eventType = event.type; 
-      
-        setTimeout(function() {
-          console.log(event.type); 
-          console.log(eventType); 
-        }, 0);
-      }
-
-      console.log(<div />)
-
-    return (
-        <nav className="navigation" onClick={onClick}>
+  return (
+        <nav className="navigation">
             <img src={PrevLogo} alt="Paul Reggie Valenzuela" />
-            Navigation
-            <p>{false}</p>
-            <p>{true}</p>
-            <p>{undefined}</p>
-            <p>{String(null)}</p>
+            <div>
+              EN | ZH
+            </div>
         </nav>
     )
 }
